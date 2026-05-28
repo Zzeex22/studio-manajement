7e18d387d8c1dc8b8e335b6c98111575b7f9e0a3
Zzeex22 <hqodri2@gmail.com>
2026-05-28 18:07:45 +0700

    Initial Laravel Project

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,16 +16,13 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next, string ...$roles): Response
     {
-        // 1. Cek dulu apakah user sudah login
         if (!Auth::check()) {
             abort(403, 'Akses ditolak! Anda belum login.');
         }
-
-        // 2. Ambil data user yang sedang login (kasih tahu VS Code ini adalah Model User)
+       
         /** @var \App\Models\User $user */
         $user = Auth::user();
-
-        // 3. Cek apakah role user tersebut ada di dalam daftar role yang diizinkan ($roles)
+        
         if (!in_array($user->role, $roles)) {
             abort(403, 'Akses ditolak! Halaman ini bukan untuk role Anda.');
         }
